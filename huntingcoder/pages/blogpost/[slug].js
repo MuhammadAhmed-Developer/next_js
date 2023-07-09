2e5700a74063f4dc437f781d23daf1f97ab20f68
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-07-09 12:06:05 -0700

    create folder structure in hunter coder

---
+++ huntingcoder/pages/blogpost/[slug].js
@@ -0,0 +1,15 @@
+import { useRouter } from 'next/router'
+import React from 'react'
+
+export default function slug() {
+
+
+    const router = useRouter()
+    const {slug} = (router.query)
+
+  return (
+    <div>
+        {slug}
+    </div>
+  )
+}
