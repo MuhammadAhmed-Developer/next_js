a3f31b4518b6e87c05dea0b74c61a77af6756bcb
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-07-12 10:03:03 -0700

    add json data in browser using api

--- huntingcoder/pages/api/blogs.js
+++ huntingcoder/pages/api/blogs.js
@@ -1,7 +1,7 @@
 import * as fs from 'node:fs';
 export default function handler(req, res) {
 fs.readFile('blogsData/learn-js.json', 'utf-8',(err, data)=>{
- console.log(data)
+ console.log()
+ res.status(200).json(JSON.parse(data))
 })
-  res.status(200).json({ name: 'John Doe' })
 }
