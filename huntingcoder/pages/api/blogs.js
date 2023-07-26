1b70b2ed857a153b13d380bb5ef09c92e017fdb9
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-07-26 11:54:59 -0700

    add promises

--- huntingcoder/pages/api/blogs.js
+++ huntingcoder/pages/api/blogs.js
@@ -1,11 +1,11 @@
 import * as fs from 'node:fs';
-export default function handler(req, res) {
-fs.readdir('blogsData',(err, data)=>{
+export default async function handler(req, res) {
+fs.promises.readdir('blogsData',(err, data)=>{
  console.log(data)
  let allblogs =[]
  data.forEach((item)=>{
  console.log(item)
- fs.readFile(('blogsData/', +  item), (d)=>{
+ fs.readFile(('blogsData/' +  item), (d)=>{
  allblogs= allblogs.push(d)
  } )                        
  })
