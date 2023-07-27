6dde9c37abc2df595a19c71b68e46ec093fdc96a
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-07-27 10:17:24 -0700

    create apis and fetch data in blogs

--- huntingcoder/pages/api/blogs.js
+++ huntingcoder/pages/api/blogs.js
@@ -1,14 +1,29 @@
-import * as fs from 'node:fs';
+import * as fs from 'fs';
 export default async function handler(req, res) {
-fs.promises.readdir('blogsData',(err, data)=>{
- console.log(data)
+    
+  let data = await  fs.promises.readdir('blogsData')
+  let myfile;
  let allblogs =[]
- data.forEach((item)=>{
- console.log(item)
- fs.readFile(('blogsData/' +  item), (d)=>{
- allblogs= allblogs.push(d)
- } )                        
- })
+
+    for (let index = 0; index < data.length; index++) {
+      const item = data[index];
+      console.log(item)
+      myfile =  await fs.promises.readFile(('blogsData/' +  item), "utf-8" )
+      // console.log(myfile)  
+       allblogs.push(JSON.parse(myfile))
+
+  }
  res.status(200).json(allblogs)
-})
+
+// fs.promises.readdir('blogsData',(err, data)=>{
+//  console.log(data)
+//  let allblogs =[]
+//  data.forEach((item)=>{
+//  console.log(item)
+//  fs.readFile(('blogsData/' +  item), (d)=>{
+//  allblogs= allblogs.push(d)
+//  })                        
+//  })
+// })
+//  res.status(200).json(allblogs)
 }
--- huntingcoder/pages/blog.js
+++ huntingcoder/pages/blog.js
@@ -1,23 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '../styles/Blog.module.css'
 import Link from 'next/link'
 export default function blog() {
+ 
+  const [blogs, setblogs] = useState([])
+
+  useEffect(() => {
+    console.log('Working');
+    fetch('http://localhost:3000/api/blogs').then((a) => {
+      return a.json();
+    })
+    .then((parsed) => {
+      
+      console.log(parsed);
+      setblogs(parsed)
+    })
+  }, [])
   return (
     <div className={styles.main}>
-    <div className={styles.blogs}>
-    <h2>Popular Blogs</h2>
-    <div className="blogitem">
-    <Link href={'/blogpost/learn-javascript-mind'}>
-      <h3>Learn JS in 2023?</h3></Link>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <h2>Popular Blogs</h2>
+        {blogs.map((blogitem)=>{
+          return  <div className={styles.blogs}>
+        
+          <div className="blogitem">
+            <Link href={'/blogpost/learn-javascript-mind'}>
+              <h3>Auther : {blogitem.author}</h3>
+              <h3>{blogitem.title}</h3></Link>
+            <p>{blogitem.content}</p>
+          </div>
+        </div>
+        })}
     </div>
-  </div> 
-    <div className="blogs">
-    <h2>Popular Blogs</h2>
-    <div className="blogitem">
-      <h3>Learn JS in 2023?</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-    </div>
-  </div> 
-    </div> 
-)}
+  )
+}
