234290effc30111871c6c8048524bf4babc663e3
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-07-28 10:14:14 -0700

    create contact api page

---
+++ huntingcoder/pages/api/postcontact.js
@@ -0,0 +1,9 @@
+export default function handler(req, res) {
+    if (req.method === 'POST') {
+      // Process a POST request
+    } else {
+      // Handle any other HTTP method
+      res.status(200).json(['Contact US'])
+    //   name, email,desc,phone
+    }
+  }
