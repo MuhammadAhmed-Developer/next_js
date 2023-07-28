8f909dc070804d24d1feab14222dce0fd93e74a2
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-07-27 21:38:42 -0700

    add slug with serverSideProps

--- huntingcoder/pages/blogpost/[slug].js
+++ huntingcoder/pages/blogpost/[slug].js
@@ -2,34 +2,46 @@ import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import styles from '../../styles/blogpost.module.css'
 
-export default function slug() {
+export default function slug(props) {
 
-  const [blog, setblog] = useState()
+  const [blog, setblog] = useState(props.myBlog)
 
-  const router = useRouter()
-  useEffect(() => {
-    if (!router.isReady) return
-    const {slug} = (router.query)
+  // const router = useRouter()
+  // useEffect(() => {
+  //   if (!router.isReady) return
+  //   const {slug} = (router.query)
 
-    fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((a) => {
-      return a.json();
-    })
-    .then((parsed) => {
-      setblog(parsed)
-    })
-  }, [router.isReady])
+  //   fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((a) => {
+  //     return a.json();
+  //   })
+  //   .then((parsed) => {
+  //     setblog(parsed)
+  //   })
+  // }, [router.isReady])
 
 
   return (
     <div className={styles.container}>
 
-    <div className={styles.main}>
+      <div className={styles.main}>
         <h1>{blog && blog.title}</h1>
         <hr />
         <div className='blog-content'>
-         {blog && blog.content}
+          {blog && blog.content}
         </div>
-    </div>
+      </div>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  // console.log(context.query)
+  // // const router = useRouter()
+  const { slug } = (context.query)
+  let data = await fetch(`http://localhost:3000/api/getblog?slug=${slug}`)
+  let myBlog = await data.json()
+  return {
+    props: { myBlog }
+  }
+
 }
