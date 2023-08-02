4692e4a55d81d6922c37b8b8c99aeba082239b61
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-08-02 12:38:42 -0700

    add tailwindcss

---
+++ sastaKharido/sastakharido/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+        <div>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="SastaKharido.com" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      heloow
+      <div className='mx-4 my-6 bg-red-600'>Hi this is tailwindcss</div>
+        
+  
+    </div>
+  )
+}
