d5f1dd654adc90d27989893ea1f9f4c35909be8b
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-08-04 11:02:17 -0700

    add tailblock card

--- sastaKharido/sastakharido/components/Navber.js
+++ sastaKharido/sastakharido/components/Navber.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { AiOutlineShoppingCart } from 'react-icons/ai';
 
 export default function Navber() {
   return (
-    <div className='flex  flex-col md:flex-row md:justify-start   justify-center items-center'>
+    <div className='flex  flex-col md:flex-row md:justify-start   justify-center items-center py-1'>
      <div className='logo mx-6 mb-2'>
-        <Image src='/logo.png' alt='logo' height={500} width={200}/>
+        <Image src='/logo.png' alt='logo' height={100} width={180}/>
      </div>
        <div className="nav">
         <ul className='flex items-center space-x-5 font-bold'>
@@ -16,8 +17,8 @@ export default function Navber() {
             <Link href={'/'}><li>Mugs</li></Link>
         </ul>
        </div>
-       <div className="cart absolute right-5 top-2">
-        <button>Cart</button>
+       <div className="cart absolute right-5 ">
+        <AiOutlineShoppingCart className='text-xl md:text-3xl cursor-pointer'/> 
        </div>
     </div>
   )
