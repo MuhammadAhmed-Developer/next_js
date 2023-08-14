0d23defc31fbcbf5cdbcf82141d99d5306c9ea3f
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-08-14 00:33:28 -0700

    add model install mongooes and add order schema

---
+++ sastaKharido/sastakharido/models/order.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const OrderSchema =  new mongoose.Schema({
+    UserId: {type: String, require: true},
+    products: [{
+          productId: {type: String},
+          quantity: {type: Number, default: 1}
+        }],
+    address: {type: String, require: true},
+    amount: {type: Number, require: true},
+    status: {type: String,  default: 'pending', require: true},
+
+  }, {timestamps:true});
