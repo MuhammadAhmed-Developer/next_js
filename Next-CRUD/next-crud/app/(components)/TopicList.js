daee75904b352e145f1ae8dc10efc0a68097b484
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-08-18 12:26:03 -0700

    fetch data form mongodb

--- Next-CRUD/next-crud/app/(components)/TopicList.js
+++ Next-CRUD/next-crud/app/(components)/TopicList.js
@@ -1,31 +1,42 @@
 import Link from "next/link";
 import RemoveBtn from "./RemoveBtn";
 import { FaEdit } from 'react-icons/fa';
-export default function TopicLists() {
 
+const getTopics = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/topics", { cache: "no-store" })
 
-  const getTopic = async () =>{
-     try {
-        const res =  await fetch("http://localhost:3000/api/topics",{cache: "no-store"})
-      
-     } catch (error) {
-       console.log(error);
-     }
+    if (!res.ok) {
+      throw new Error("Failed To Fetch topics")
+    }
+    return res.json()
   }
-  
+  catch (error) {
+    console.log(error);
+  }
+}
+
+export default async function TopicLists() {
+
+  const { topics } = await getTopics()
+
   return (
-    <div className="px-5 py-3 flex justify-between border border-slate-500 my-3 gap-5 items-start align-middle">
-      <div>
-        <h1 className="font-bold text-2xl">Title</h1>
-        <h2>Description</h2>
-      </div>
-      <div className="flex gap-3 py-3">
-        <RemoveBtn />
-        <Link href={'/editTopic/123'}>
-          <FaEdit size={24}  className="text-blue-500"/>
-          </Link>
-      </div>
-    </div>
+    <>
+      {topics.map((t) => (
+        <div className="px-5 py-3 flex justify-between border border-slate-500 my-3 gap-5 items-start align-middle">
+          <div>
+            <h1 className="font-bold text-2xl">{t.title}</h1>
+            <h2>{t.description}</h2>
+          </div>
+          <div className="flex gap-3 py-3">
+            <RemoveBtn />
+            <Link href={`/editTopic/${t._id}`}>
+              <FaEdit size={24} className="text-blue-500" />
+            </Link>
+          </div>
+        </div>
+      ))}
+    </>
 
 
   )
